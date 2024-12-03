3baef1e8e164bb667b7a0a9b64ee58d3af3c6f94
vilker <francisco.lopes275@aluno.ce.gov.br>
2024-12-03 09:27:06 -0500

    q

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-// Adiciona a classe 'active' ao botão para ativar a animação
-document.querySelector('.menu-btn.animated').addEventListener('click', function() {
-    this.classList.toggle('active');
-    let bo = document.getElementById("meuBotao")
-    bo.addEventListener('click', function() {
-        // Este código será executado quando o botão for clicado
-        alert('Você clicou no botão!');
-      });
-    
-  });
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
   
